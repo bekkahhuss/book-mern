52bd5a10685d1e5e905e00034ae898d9a1c86b80
bekkahhuss <bekkahhuss@gmail.com>
2022-05-27 22:10:35 -0400

    query and mutation working! updated resolvers and typedefs

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,9 +1,30 @@
+const User = require("../models/User");
+const { signToken } = require('../utils/auth');
+
+// const {User} = require("../models")
 const resolvers = {
     Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+        me: async (parent, args, context) => {
+            if (context.user) {
+              const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+              return userData;
+            }
+      
+            throw new AuthenticationError('Not logged in');
+        }
+
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+        }
     }
+
   };
   
   module.exports = resolvers;
--- Develop/server/schemas/typedefs.js
+++ Develop/server/schemas/typedefs.js
@@ -3,10 +3,26 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+  type User {
+    _id: ID 
+    username: String 
+    email: String 
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
   type Query {
-    helloWorld: String
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
   }
 `;
+
 
 // export the typeDefs
 module.exports = typeDefs;
