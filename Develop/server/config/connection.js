71eaf59b15431062a033c4fd8457ae60a248c467
bekkahhuss <bekkahhuss@gmail.com>
2022-05-29 22:23:24 -0400

    saving for good measure

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks', {
   useNewUrlParser: true,
+  useCreateIndex: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
