b15ffcbf9c007a81e5a4a30b1a7baadf488cad0f
bekkahhuss <bekkahhuss@gmail.com>
2022-05-30 13:40:36 -0400

    heroku deploy

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
+
+
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
